7583e070c7525547656c0556549aeaae3cd97c37
koveangeles-stack <koveangeles@gmail.com>
2026-08-15 23:39:29 +0800

    Update q1_sg2_a3.py

--- q1_sg2_a3.py
+++ q1_sg2_a3.py
@@ -1,12 +1,12 @@
-i == 
-ii == 
-iii == 
+i == Rat (鼠 / Shǔ)
+ii == Ox (牛 / Niú)
+iii == Tiger (虎 / Hǔ
 iv == Rabbit (兔 / Tù)
 v == Dragon (龙 / Lóng)
 vi == Snake (蛇 / Shé)
 vii == Horse (马 / Mǎ)
 viii == Goat (羊 / Yáng)
-ix ==
-x == 
+ix == Monkey (猴 / Hóu)
+x == Rooster (鸡 / Jī)
 xi ==
 xii ==
