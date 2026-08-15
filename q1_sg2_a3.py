307ea5a232a6b3000cc8a80cd565363615cc9730
koveangeles-stack <koveangeles@gmail.com>
2026-08-15 23:42:23 +0800

    Update q1_sg2_a3.py

--- q1_sg2_a3.py
+++ q1_sg2_a3.py
@@ -1,6 +1,6 @@
 i == Rat (鼠 / Shǔ)
 ii == Ox (牛 / Niú)
-iii == Tiger (虎 / Hǔ
+iii == Tiger (虎 / Hǔ)
 iv == Rabbit (兔 / Tù)
 v == Dragon (龙 / Lóng)
 vi == Snake (蛇 / Shé)
@@ -8,5 +8,15 @@ vii == Horse (马 / Mǎ)
 viii == Goat (羊 / Yáng)
 ix == Monkey (猴 / Hóu)
 x == Rooster (鸡 / Jī)
-xi ==
-xii ==
+xi == Dog (狗 / Gǒu)
+xii == Pig (猪 / Zhū)
+
+birthyear = int(input("Enter your birth year: "))
+
+if birthyear < 1900:
+   print("
+   break
+
+if birthyear < 1900:
+   rem = int(birthyear % 12)
+  
